970e7f0baf175be3167b92538555ecdaf4a4495d
Luminuszz <luminuszz43@gmail.com>
2021-06-09 18:09:49 -0300

    innital commit

--- src/index.js
+++
@@ -1,9 +0,0 @@
-const commander = require("commander");
-const shelljs = require("shelljs");
-const inquirer = require("inquirer");
-
-const UltronTerminalExecutor = require("./cli");
-
-const commanderInstance = new commander.Command();
-
-UltronTerminalExecutor.getInstance(commanderInstance, shelljs, inquirer);
